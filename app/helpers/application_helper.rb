ff8494906989643a5e3bc7db7752401399711912
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-04-11 17:36:14 +0530

    flash messages added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
-    
+    def flash_custom_css(key)
+        case key
+        when "notice" then "alert alert-primary"
+        when "alert" then "alert alert-danger"
+        end
+    end
     
 end

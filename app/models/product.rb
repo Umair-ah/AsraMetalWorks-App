0f1cebc53eb571168f1ad420d0c6b668027320f9
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-04-11 15:40:09 +0530

    show page completed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :admin
-  has_one_attached :image
+ 
   has_many_attached :pics
 
-  enum furniture_style: { 
-    Metal_Racks_Furniture: "Metal_Racks_Furniture", 
-    Metal_Office_Furniture: "Metal_Office_Furniture", 
-    Metal_School_Furniture: "Metal_School_Furniture"
-  }
 end
